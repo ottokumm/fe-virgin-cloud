6c5ed824b544b0e58181f95c93c988696ed036d0
Otto <Otto>
2018-09-22 10:21:41 +0400

    Adds devServer port settings

--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,48 @@
-const HtmlWebPackPlugin = require("html-webpack-plugin");
+const HtmlWebPackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: "./src/app/index.js",
-    resolve: {
-        extensions: ['.js', '.jsx', 'json'],
-    },
-    module: {
-        rules: [{
-            test: /\.(js|jsx)$/,
-            use: [
-                'babel-loader',
-                "eslint-loader"
-            ],
-            exclude: [
-                /node_modules/,
-                /\.(test|spec|e2e)\.(js|jsx)$/
-            ]
-        }, {
-            test: /\.html$/,
-            use: [{
-                loader: "html-loader"
-            }]
-        }, {
-            test: /\.scss$/,
-            use: [
-                "style-loader", // creates style nodes from JS strings
-                "css-loader", // translates CSS into CommonJS
-                "sass-loader" // compiles Sass to CSS, using Node Sass by default
-            ]
-        }, {
-            test: /\.(svg)([\?]?.*)$/,
-            use: {
-                loader: 'url-loader',
-            }
-        }]
-    },
-    plugins: [
-        new HtmlWebPackPlugin({
-            template: "./src/app/index.html",
-            filename: "./index.html"
-        })
-    ]
+  entry: './src/app/index.js',
+  resolve: {
+    extensions: ['.js', '.jsx', 'json'],
+  },
+  module: {
+    rules: [{
+      test: /\.(js|jsx)$/,
+      use: [
+        'babel-loader',
+        'eslint-loader',
+      ],
+      exclude: [
+        /node_modules/,
+        /\.(test|spec|e2e)\.(js|jsx)$/,
+      ],
+    }, {
+      test: /\.html$/,
+      use: [{
+        loader: 'html-loader',
+      }],
+    }, {
+      test: /\.scss$/,
+      use: [
+        'style-loader', // creates style nodes from JS strings
+        'css-loader', // translates CSS into CommonJS
+        'sass-loader', // compiles Sass to CSS, using Node Sass by default
+      ],
+    }, {
+      test: /\.(svg)([\?]?.*)$/,
+      use: {
+        loader: 'url-loader',
+      },
+    }],
+  },
+  devServer: {
+    inline: true,
+    port: 5999,
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './src/app/index.html',
+      filename: './index.html',
+    }),
+  ],
 };
